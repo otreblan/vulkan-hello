b22adf2b34350be8daf0b23ca6b75b1ce128a329
otreblan <otreblain@gmail.com>
2023-10-11 21:28:01 -0500

    fix(vertex): Fix color format

--- src/vertex.cpp
+++ src/vertex.cpp
@@ -19,7 +19,7 @@
 const std::array<vk::VertexInputAttributeDescription, 3> Vertex::attributeDescriptions
 {
 	vk::VertexInputAttributeDescription(0, 0, vk::Format::eR32G32Sfloat, offsetof(Vertex, pos)),
-	vk::VertexInputAttributeDescription(1, 0, vk::Format::eR32G32Sfloat, offsetof(Vertex, color)),
+	vk::VertexInputAttributeDescription(1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color)),
 	vk::VertexInputAttributeDescription(2, 0, vk::Format::eR32G32Sfloat, offsetof(Vertex, texCoord)),
 };
 
